15ea87fd27b52428512cb25f625e7a991053d70b
Ignacio Fuentes <ignacio.fuentes@genesys.cl>
2023-06-12 21:20:51 -0400

    arerglo front

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   apiUrl: "http://44.209.216.136:3000/",
 };
 
