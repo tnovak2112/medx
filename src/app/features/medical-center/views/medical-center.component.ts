b01829c1061ba5c8a8f523c08a9b83ffe3c295c8
Tom <tnovakovic.dev@gmail.com>
2023-06-24 21:49:22 -0400

    creacion medical center

--- src/app/features/medical-center/views/medical-center.component.ts
+++ src/app/features/medical-center/views/medical-center.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from "@angular/core";
+import { 
+  faLocationDot, 
+  faSquarePhone,
+  faLink,
+  faShareNodes 
+} from "@fortawesome/free-solid-svg-icons";
+import { faSquareWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: "app-medical-center",
@@ -6,6 +13,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./medical-center.component.css"],
 })
 export class MedicalCenterComponent implements OnInit {
+  
+  locationIcon = faLocationDot;
+  phoneIcon = faSquarePhone;
+  whatsappIcon = faSquareWhatsapp;
+  linkIcon = faLink;
+  shareIcon = faShareNodes;
+
   constructor() {}
 
   ngOnInit() {}
